53c51a49a7805e75dbd8d162f131c876174b9084
supercoolguy0 <jasper.waite.00@education.tas.edu.au>
2026-09-23 09:44:35 +1000

    update

---
+++ Assets/PlayerHealth1.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour
+{
+    public int health;
+    public int maxHealth = 10;
+    void Start()
+    {
+        health = maxHealth;
+    }
+    public void TakeDamage(int amount)
+    {
+        health -= amount;
+        if (health <= 0)
+        {
+            Destroy(gameObject);
+        }
